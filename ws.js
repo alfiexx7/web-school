a3d4f781fdb54f54ce4d4f217c9e496d2c444bc8
alfiexx7 <120137723+alfiexx7@users.noreply.github.com>
2022-12-09 12:49:23 +0300

    Add files via upload

---
+++ ws.js
@@ -0,0 +1,8 @@
+const hamburger = document.querySelector('.hamburger');
+const navbar = document.querySelector('ul');
+
+hamburger.addEventListener('click', ()=>{
+	navbar.classList.toggle('slide')
+});
+
+
